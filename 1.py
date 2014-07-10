4e1ed6f46a22bbc79b5a254f72186c0d8e6b4d40
Yao <zhaox383@umn.edu>
2014-07-10 10:29:42 -0500

    finished 1

--- 1.py
+++ 1.py
@@ -2,16 +2,45 @@ class Solution:
 	# @param s, a string
 	# @return a string
 	def reverseWords(self, s):
+		first = True
+		repeat = False
 		j = 0
 		result = ""
 		temp_word = ""
-		for i in range(len(s)):
-			temp_word[j++] = s[i]
+		s = s.strip()
+		if s == "":
+			return result
+		for i in s:
 			# cache character
-			if s[i] == " ":
-				for k in range(len(temp_word)):
-					
-				temp_word = ""
+			if i == " ":
+				if repeat:
+					continue
 				# put word in new string
+				if first:
+					result = temp_word + result	
+					first = False
+				else:
+					result = temp_word + " " + result	
+				temp_word = ""
+				repeat = True
+			else:
+				repeat = False
+				temp_word += i
+
+		if first:
+			result = temp_word + result	
+		else:
+			result = temp_word + " " + result	
+		return result
+
+def test():
+	a = Solution()
+	print a.reverseWords("the sky is blue") + "#"
+	print a.reverseWords("     ") + "#"
+	print a.reverseWords("a") + "#"
+
+if __name__ == '__main__':
+	test()
+
 				
 
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 151 Leet Code Question:
 
 Progress:
-1 Reverse Words in a String ongoing 
-
-solved : 0/151
+1 Reverse Words in a String Done 
+2 Evaluate Reverse Polish Notation 
+ 
+solved : 1/151
