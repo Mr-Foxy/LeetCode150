36603d6298694e2c8be8d35eec9c55176fdc8a8f
Mr.Fox <zhaox383@umn.edu>
2014-07-12 11:37:39 -0500

    4 solved

--- 4.py
+++ 4.py
@@ -1,3 +1,4 @@
+import pdb
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, x):
@@ -16,9 +17,9 @@ def push(head, x):
 
 def print_list(head):
 	current = head
-	while current.next != None:
-		current = current.next
+	while current != None:
 		print current.val
+		current = current.next
 	
 class Solution:
 	# @param head, a ListNode
@@ -32,26 +33,38 @@ class Solution:
 	def partition(self, head, tail):
 		pivot_val = tail.val
 		current = head
-		less_pivot = head
+		less_pivot = ListNode(-1)
+		less_pivot.next = head
 		first = True
+		pdb.set_trace()
 		while current != tail:
 			if current.val < pivot_val:
-				if first:
-					first = False
-				else:
-					less_pivot = less_pivot.next
+				less_pivot = less_pivot.next
 				less_pivot.val, current.val = current.val, less_pivot.val
 			current = current.next
-
 		less_pivot.next.val, tail.val = tail.val, less_pivot.next.val
-		return less_pivot.next
+			
+		return less_pivot, less_pivot.next
 
 	def quicksort_recur(self, head, tail):
-		if head != tail:
-			pivot = None
-			pivot = self.partition(head, tail)
-			
-			self.quicksort_recur(head, pivot)
+		#pdb.set_trace()
+		'''
+		cur = tail
+		while cur != None:
+			if cur == head:
+				return
+			cur = cur.next
+		'''
+		if tail.next == head or tail == head:
+			return
+		prev, pivot = self.partition(head, tail)
+		print "head is {}".format(head.val)
+		print "tail is {}".format(tail.val)
+		print "pivot is {}".format(pivot.val)
+		print_list(head)
+		
+		self.quicksort_recur(head, prev)
+		if tail != pivot:
 			self.quicksort_recur(pivot.next, tail)
 		
 	def sortList(self, head):
@@ -65,12 +78,10 @@ def main():
 	push(new_head, 15)
 	push(new_head, 7)
 	push(new_head, 6)
-	print_list(new_head)
 	a = Solution()
 	a.sortList(new_head)
+	print "result is :"
 	print_list(new_head)
 	
 if __name__ == '__main__':
 	main()
-		
-			
